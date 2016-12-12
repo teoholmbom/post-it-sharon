6cf36b589c0eddac33fd292e0c14e1cf6940ace8
teoholmbom <holteo@gmail..com>
2016-12-12 14:42:57 +0100

    Second commit!

--- post-it-sharon.py
+++ post-it-sharon.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from food import Food
 import pickle
 
@@ -6,21 +6,29 @@ foods = ();
 
 app = Flask(__name__)
 
+def createPickle():
+    # Create ist with objects of Food
+    temp_foods = [
+        Food('Banana', 200, 'Southeast Asia', 'Is a herb.'),
+        Food('Watermelon', 30, 'Southern Africa', 'Worlds biggest berry.'),
+        Food('Kale', 50, 'Greece', 'The closest relative to wild cabbage.')
+    ]
+    # Create a pickle containing temp_foods list
+    pickle.dump(temp_foods, open("save.p", "wb"))
 
-@app.route('/')
-def show_foods():
+
+@app.route('/', methods=['GET', 'POST'])
+def foods():
+    if request.method == 'POST':
+        temp_food = Food(request.form['name'], request.form['energy'], 'N/A', request.form['fact'])
+        foods.append(temp_food)
+        pickle.dump(foods, open("save.p", "wb"))
+
     return render_template('show_foods.html', foods=foods)
 
 
-if __name__ == '__main__':
-    # Tuple with objects of Food
-    # foods0 = (
-    #    Food('Banana', 200, 'Southeast Asia', 'Is a herb.'),
-    #    Food('Watermelon', 30, 'Southern Africa', 'Worlds biggest berry.'),
-    #    Food('Kale', 50, 'Greece', 'The closest relative to wild cabbage.')
-    # )
-    # pickle.dump(foods0, open("save.p", "wb"))
 
+if __name__ == '__main__':
+    createPickle()
     foods = pickle.load(open("save.p", "rb"))
-    print(foods)
-    app.run()
+    app.run(debug=True)
