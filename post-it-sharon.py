448bee7849bcdfbc88a82525f3529027706a5a46
teoholmbom <holteo@gmail..com>
2016-12-13 12:52:06 +0100

    Some minor edits!

--- post-it-sharon.py
+++ post-it-sharon.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from food import Food
 import pickle
 
-foods = ();
+foods = []
 
 app = Flask(__name__)
 
-def createPickle():
+
+def create_pickle():
     # Create ist with objects of Food
     temp_foods = [
         Food('Banana', 200, 'Southeast Asia', 'Is a herb.'),
@@ -17,18 +18,32 @@ def createPickle():
     pickle.dump(temp_foods, open("save.p", "wb"))
 
 
-@app.route('/', methods=['GET', 'POST'])
-def foods():
-    if request.method == 'POST':
-        temp_food = Food(request.form['name'], request.form['energy'], 'N/A', request.form['fact'])
-        foods.append(temp_food)
-        pickle.dump(foods, open("save.p", "wb"))
-
+@app.route('/')
+def display_foods():
     return render_template('show_foods.html', foods=foods)
 
 
+@app.route('/food/add', methods=['POST'])
+def add_food():
+    # Create new Food object with data from form
+    temp_food = Food(request.form['name'], request.form['energy'], request.form['origin'], request.form['fact'])
+    foods.append(temp_food)
+    pickle.dump(foods, open("save.p", "wb"))
+    return redirect(url_for('display_foods'))
+
 
 if __name__ == '__main__':
-    createPickle()
-    foods = pickle.load(open("save.p", "rb"))
+    while True:
+        try:
+            foods = pickle.load(open("save.p", "rb"))
+            break
+        except EOFError:
+            create_pickle()
+        except ImportError:
+            create_pickle()
+        except IndexError:
+            create_pickle()
+        except FileNotFoundError:
+            create_pickle()
+
     app.run(debug=True)
